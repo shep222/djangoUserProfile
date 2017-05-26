2cf911ec1326dda0fafaaf4c9c1f4e3db6fc27c3
John Shepler <john.sheplerjr@gmail.com>
2017-05-26 15:37:49 -0600

    added improved permission to user profile feed items API

--- src/profiles_project/profiles_api/admin.py
+++ src/profiles_project/profiles_api/admin.py
@@ -4,4 +4,4 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.UserProfile)
-admin.stie.register(models.ProfileFeedItem)
+admin.site.register(models.ProfileFeedItem)
